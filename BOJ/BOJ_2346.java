ec8f3ccd802f2d4ad1cefe7b716aa642a5a11a8c
LinkinP7 <qkrwlstjd3@naver.com>
2022-06-15 02:44:18 +0900

    home#001_220614

--- BOJ/BOJ_2346.java
+++ BOJ/BOJ_2346.java
@@ -6,17 +6,26 @@ public class BOJ_2346 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         List<Integer> arr = new ArrayList<Integer>();
+        int[] check = new int[N];
 
         int[] answer = new int[N];
-        for(int i=0;i<N;i++)
+        for(int i=0;i<N;i++){
             arr.add(sc.nextInt());
+            check[i] = arr.get(arr.size()-1);
+        }
         sc.close();
 
-        int idx = 0;
-        // for(int i=0;i<N;i++){
-        //     answer[i] = idx+1;
-        //     idx = arr[idx];
-        // }
+        answer[0] = 1;
+
+        int idx = 0, x=1;
+        for(int i=1;i<N-1;i++){
+            if(arr.get(idx) > 0){
+                answer[x] = idx+arr.get(idx);
+            }else{
+                answer[x] = N - Math.abs(idx-arr.get(idx)) - 1;
+            }
+            x++;
+        }
 
         System.out.println(arr);
         System.out.println(Arrays.toString(answer));
