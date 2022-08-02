174d5618257dd8f3c38a0c94477359e2acc289d8
LinkinP7 <qkrwlstjd3@naver.com>
2022-08-03 00:31:31 +0900

    home#004_220802

--- INF/Stack_Queue/INF_Stack_08.java
+++ INF/Stack_Queue/INF_Stack_08.java
@@ -4,19 +4,27 @@ import java.util.*;
 class INF_Stack_08{
 	public int solution(int N, int K, int[] arr){
         int answer = 0;
+        int key = arr[K];
+        int idx = K;
         Queue<Integer> q = new LinkedList<>();
         for(int x : arr) q.offer(x);
-        
-        while(!q.isEmpty()){   // 2중이라 돼도 좀 그렇네....
+        while(!q.isEmpty()){  
             int x = q.poll();
+            boolean flag = true;
             for(int y : q){
                 if(x < y){
                     q.offer(x);
+                    flag = false;
                     break;
                 } 
             }
+            if(flag){
+                answer++;
+                if(x == key && idx == 0) break;
+            }
+            if(idx == 0) idx = q.size()-1;
+            else idx--; 
         }
-        
 		return answer;
 	}
 	public static void main(String[] args){
@@ -33,6 +41,49 @@ class INF_Stack_08{
 	}
 }
 
+// #1 같은 숫자인 경우 순서 계산이 되지 않는다.
+// class INF_Stack_08{
+// 	public int solution(int N, int K, int[] arr){
+//         int answer = 0;
+//         int key = arr[K];
+//         Queue<Integer> q = new LinkedList<>();
+//         for(int x : arr) q.offer(x);
+//         while(!q.isEmpty()){  
+//             int x = q.poll();
+//             boolean flag = true;
+//             for(int y : q){
+//                 if(x < y){
+//                     q.offer(x);
+//                     flag = false;
+//                     break;
+//                 } 
+//             }
+//             if(flag){
+//                 answer++;
+//                 if(x == key) break;
+//             } 
+//         }
+// 		return answer;
+// 	}
+// 	public static void main(String[] args){
+// 		INF_Stack_08 T = new INF_Stack_08();
+// 		Scanner sc = new Scanner(System.in);
+// 		int N = sc.nextInt();
+//         int K = sc.nextInt();
+//         int[] arr = new int[N];
+//         for(int i=0;i<N;i++)
+//             arr[i] = sc.nextInt();
+
+// 		System.out.println(T.solution(N,K,arr));  
+//         sc.close();
+// 	}
+// }
+
+// 6 0
+// 60 60 90 60 60 60    ->   5 가 나와야 하는데 2가 나온다
+
+
+
 
 // 메디컬 병원 응급실에는 의사가 한 명밖에 없습니다.
 // 응급실은 환자가 도착한 순서대로 진료를 합니다. 하지만 위험도가 높은 환자는 빨리 응급조치를 의사가 해야 합니다.
@@ -46,3 +97,7 @@ class INF_Stack_08{
 
 // 5 2
 // 60 50 70 80 90       ->    3
+
+// 100 25
+// 70 81 58 71 62 68 92 63 50 53 53 61 77 96 67 60 74 54 91 81 65 65 82 50 95 98 87 93 70 72 90 62 64 99 74 73 67 95 71 82 61 96 57 84 84 91 52 50 88 90 77 54 50 70 52 99 99 83 72 57 78 61 54 59 58 78 79 75 84 78 95 61 61 50 90 76 51 55 64 99 50 72 61 77 72 93 62 87 76 57 62 74 94 71 98 84 63 74 73 68 
+// -> 6
