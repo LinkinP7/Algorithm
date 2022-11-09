ba430bfa05e3d1bc780b40c50a30d17a1090c099
linkinp7 <qkrwlstjd3@naver.com>
2022-11-09 18:09:52 +0900

    work#032_221109

---
+++ BOJ/Silver/BOJ_1406.java
@@ -0,0 +1,88 @@
+package BOJ.Silver;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
+// 실버 2 에디터
+public class BOJ_1406 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        char[] input = br.readLine().toCharArray();
+        LinkedList<Character> list = new LinkedList<>(); 
+        ListIterator<Character> iter = list.listIterator();
+        StringTokenizer st;
+
+        for (Character c : input) 
+            iter.add(c);
+        
+        int cursor = list.size();
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-->0){
+            st = new StringTokenizer(br.readLine());
+            String x = st.nextToken();
+            switch(x){
+                case "L": if(iter.hasPrevious()) iter.previous(); break;
+                case "D": if(iter.hasNext()) iter.next(); break;
+                case "B": if(iter.hasPrevious()){
+                            iter.previous();
+                            iter.remove();
+                          } break;
+                case "P": iter.add(st.nextToken().charAt(0));
+                          break;
+            }
+            // System.out.println("NOWCURSOR::: " + cursor);
+        }
+        // System.out.println(list);
+        for (Character c : list) 
+            sb.append(c);
+        System.out.println(sb);
+    }
+}
+
+
+// 시간초과...
+// List의 add나 remove는 0부터 찾아들어가서 그 부분에서 동작한뒤 더하거나 빼면 다른 원소들이 전부 한칸씩 움직이는 연산을 하게된다.
+// O(N) 이 되어버림.
+// Stack 혹은 ListIterator 로 풀어야 한다.
+// ArrayList는 정렬할 때만 사용하고, 이렇게 추가하고, 제거하는 문제의 경우엔 LinkedList를 활용하도록 하자.
+// LinkedList에서 remove와 add는 리스트의 처음부터 끝까지 탐색한다는 걸 잊지말자.
+// ListIterator는 양방향 이동이 가능하나 ArrayList, LinkedList 에서만 사용이 가능하다.
+
+// public class BOJ_1406 {
+//     public static void main(String[] args) throws Exception{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringBuilder sb = new StringBuilder();
+//         char[] input = br.readLine().toCharArray();
+//         ArrayList<Character> list = new ArrayList<>(); 
+//         StringTokenizer st;
+
+//         for (Character c : input) 
+//             list.add(c);
+        
+//         int cursor = list.size();
+//         int T = Integer.parseInt(br.readLine());
+
+//         while(T-->0){
+//             st = new StringTokenizer(br.readLine());
+//             String x = st.nextToken();
+//             switch(x){
+//                 case "L": if(cursor>=1) cursor--; break;
+//                 case "D": if(cursor<list.size()) cursor++; break;
+//                 case "B": if(cursor != 0){
+//                             list.remove(cursor-1); cursor--;
+//                           } break;
+//                 case "P": char a = st.nextToken().charAt(0);
+//                           list.add(cursor, a); cursor++;
+//                           break;
+//             }
+//             // System.out.println("NOWCURSOR::: " + cursor);
+//         }
+//         // System.out.println(list);
+//         for (Character c : list) 
+//             sb.append(c);
+//         System.out.println(sb);
+//     }
+// }
---
+++ BOJ/Silver/BOJ_1764.java
@@ -0,0 +1,37 @@
+package BOJ.Silver;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+// 실버 4 듣보잡
+public class BOJ_1764 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken()); // 듣도못한
+        int M = Integer.parseInt(st.nextToken()); // 보도못한
+        Set<String> heard = new HashSet<>();
+        ArrayList<String> HS = new ArrayList<>();
+
+        while(N-->0)
+            heard.add(br.readLine());
+        
+        while(M-->0){
+            String name = br.readLine().toString();
+            if(heard.contains(name)) HS.add(name);
+        }
+
+        Collections.sort(HS);
+        sb.append(HS.size()).append("\n");
+        for (String string : HS) {
+            sb.append(string).append("\n");
+        }
+
+        System.out.println(sb);    
+    }
+}
